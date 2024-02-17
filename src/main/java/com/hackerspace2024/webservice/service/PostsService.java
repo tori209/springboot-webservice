a596fd60f5033c03530d319e48331f6142e8d260
tori209 <kyoungho2018@gist.ac.kr>
2024-02-17 14:19:02 +0900

    [add] 게시글 목록 구현

--- src/main/java/com/hackerspace2024/webservice/service/PostsService.java
+++ src/main/java/com/hackerspace2024/webservice/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.hackerspace2024.webservice.service;
 
 import com.hackerspace2024.webservice.domain.posts.PostsRepository;
+import com.hackerspace2024.webservice.dto.posts.PostsMainResponseDto;
 import com.hackerspace2024.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -14,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/hackerspace2024/webservice/web/WebController.java
+++ src/main/java/com/hackerspace2024/webservice/web/WebController.java
@@ -1,15 +1,20 @@
 package com.hackerspace2024.webservice.web;
 
+import com.hackerspace2024.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
 
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String mainDo() {
+    public String mainDo(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
